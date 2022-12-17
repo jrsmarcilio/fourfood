97552f7c9a47b632f5e38a593a44e5c4dfc31f92
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2022-12-17 11:03:29 -0300

    Atualização de splashscreen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ClienteView from './src/screens/CadastroClienteView';
 import EmpresaView from './src/screens/CadastroEmpresaView';
+import FlashMessage from 'react-native-flash-message';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
         <Stack.Screen name='ClienteView' component={ClienteView}/>
         <Stack.Screen name='EmpresaView' component={EmpresaView}/>
       </Stack.Navigator>
+      <FlashMessage position={'bottom'}/>
     </NavigationContainer>
   );
 }
